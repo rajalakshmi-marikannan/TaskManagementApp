f773cc651371ebb3a260bad22ddfad82c8edd328
Rajalakshmi Marikannan <rajalakshmimarikannan@gmail.com>
2025-02-11 03:43:14 +0530

    Rajalakshmi - Internship assignment Implemented unit, integration, and E2E tests
    - Implemented integration tests for API interactions.
    - Created E2E tests covering task and user management.
    - Fixes vulnerabilities in undici and vite dependencies.

--- TaskManagementApp/src/app/helpers/calculations.spec.ts
+++ TaskManagementApp/src/app/helpers/calculations.spec.ts
@@ -1,37 +1,46 @@
 import Calculations from "./calculations";
 
-// Please complete the following unit tests for the Calculations class:
-
-describe('Calculations', () =>
-{
-    it('should calculate the percentage standard deviation of a one-dimensional array', () =>
-    {
-        // TODO: Implement test
-
+describe('Calculations', () => {
+    it('should calculate the percentage standard deviation of a one-dimensional array', () => {
+        const array = [1, 4, 6, 7]
+        const result = Calculations.calculatePercentageStandardDeviationOfOneDimensionalArray(array)
+        expect(result).toBeCloseTo(50.91750, 3);
     });
 
 
-    it('should calculate the zero crossing percentage of an array', () =>
-    {
-        // TODO: Implement test
-
+    it('should calculate the zero crossing percentage of an array', () => {
+        const array = [-5, -1, 1, 4, 6, 7]
+        const result = Calculations.zeroCrossingPercentage(array)
+        expect(result).toBeCloseTo(16.666666, 3);
     });
 
-    it('should calculate the average of a two-dimensional array', () =>
-    {
-        // TODO: Implement test
-
+    it('should calculate the average of a two-dimensional array', () => {
+        const array = [
+            [1, 4, 6, 7],
+            [4, 1, 6, 7],
+            [7, 6, 4, 1],
+        ];
+        const result = Calculations.calculateAverageOfTwoDimensionalArray(array)
+        expect(result).toBeCloseTo(4.5, 1);
     });
 
-    it('should get the most frequent value at each subarray', () =>
-    {
-        // TODO: Implement test
-
+    it('should get the most frequent value at each subarray', () => {
+        const array = [
+            [1, 4, 6, 1],
+            [4, 1, 6, 4],
+            [7, 7, 4, 1],
+        ];
+        const result = Calculations.getMostFrequentValueAtEachSubarray(array)
+        expect(result).toEqual([1, 4, 7]);
     });
 
-    it('should get the average value of each subarray after trimming percentage highest and lowest values', () =>
-    {
-        // TODO: Implement test
-
+    it('should get the average value of each subarray after trimming percentage highest and lowest values', () => {
+        const array = [
+            [1, 4, 6, 7],
+            [4, 1, 6, 7],
+            [7, 6, 4, 1],
+        ];
+        const result = Calculations.getAverageValueOfEachSubarryAfterTrimmingPercentageHighestAndLowestValues(array, 25)
+        expect(result).toEqual([5, 5, 5]);
     });
 });
